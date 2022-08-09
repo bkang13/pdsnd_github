ca4c289662b717aac4a610101c09356d798bd31a
bkang13 <bkang13@ford.com>
2022-08-09 13:30:50 -0400

    refactoring commit 3: added comment "refactoring commit 3"

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -23,7 +23,7 @@ def get_filters():
     #refactoring commit 2
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
+    #refactoring commit 3
 
     print('-'*40)
     return city, month, day
